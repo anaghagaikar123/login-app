48acd3f7f3673c7e5bd1a9f8e98b1f57a5f48ad7
Anagha <anagha.a.gaikar@gmail.com>
2019-06-25 18:46:30 +0530

    add components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //styleUrls: ['./app.component.css']
+  styles : [`
+    h2{
+    color : aqua;
+    }
+    `
+  ]
 })
 export class AppComponent {
   title = 'Login application';
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,13 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-login',
+  selector : 'app-login',
+ // selector: '[app-login]', //we provide selector as attribute
+ // selector : '.app-login', //we provide selector as class
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-    title = 'anagha';
-  constructor() { }
+    
+  userId: number = 147;
+  username: String = 'anagha';
+  newUser:  String = 'vipul';
+  getUserName()
+  {
+    return this.username;
+  }
+
+  constructor() {
+  
+   }
 
   ngOnInit() {
   }
